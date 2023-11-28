bfb61f0aad3a0f0990d640bb9de896639c66c5ff
Diana <150387973+shadriova@users.noreply.github.com>
2023-11-29 00:29:12 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,16 +1,24 @@
 import telebot
-bot = telebot.TeleBot('6424481048:AAHAbiskuJtXC_PiCWzBHiNS8Gjq7A8_NxM')
- 
-@bot.message_handler(commands=['/prediction1'])
-def main(message):
-    bot.send_message(message.chat.id, 'вам всегда будет светить солнце и ваши дни будут полны красок', parse_mode='Markdown')
+bot = telebot.TeleBot('6823637731:AAGBk7Ca_UVnMIHEu3O69xDG7DmK4DiFMo4')
 
-@bot.message_handler(commands=['/prediction2'])
+@bot.message_handler(commands=['start'])
 def main(message):
-    bot.send_message(message.chat.id, 'сияние звезд озарит вам путь и укажет верное направление', parse_mode='Markdown')
-
-@bot.message_handler(commands=['/prediction3'])
-def main(message):
-    bot.send_message(message.chat.id, 'для вас успех и слава, равносильно смене дня и ночи ', parse_mode='Markdown')
+    bot.send_message(message.chat.id, 'ПРИВЕТ! \nдля начала работы оплатите бота)')
     
+@bot.message_handler(commands=['pay'])
+def main(message):
+    bot.send_message(message.chat.id, '-500$ \nвы успешно оплатили бота!')
+    
+@bot.message_handler(commands=['meme'])
+def main(message):
+    bot.send_message(message.chat.id, '*ТЫК*(https://youtu.be/dQw4w9WgXcQ?si=aJHvo7eFfa6OHYPx)', parse_mode='Markdown')
+
+@bot.message_handler(commands=['joke'])
+def main(message):
+    bot.send_message(message.chat.id, 'Штирлиц шёл в Дрезден с трудом разбирая дорогу. \nНаутро железная дорога от Берлина до Дрездена была полностью разобрана...',)
+
+@bot.message_handler(commands=['song'])
+def main(message):
+    bot.send_message(message.chat.id, '*ТЫК*(https://youtu.be/boT7QIGOmbc?si=AC9VetUeUMHdFPDK)', parse_mode='Markdown')
+
 bot.infinity_polling()
