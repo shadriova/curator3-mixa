0b473656d6a0150dd36a07c47432ef66d1beee75
Diana <150387973+shadriova@users.noreply.github.com>
2023-11-28 23:19:51 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,29 +1,21 @@
 import telebot
-from telebot import types
-from datetime import datetime
- 
-bot = telebot.TeleBot('your_token')
- 
- 
-def get_cat_url():
-    return f'https://cataas.com/cat?t={int(datetime.now().timestamp())}'
- 
- 
-@bot.message_handler(commands=['start'])
-def handle_start(message):
-    markup = types.ReplyKeyboardMarkup(resize_keyboard=True)
-    item1 = types.KeyboardButton("Котик")
-    markup.add(item1)
- 
-    bot.send_message(message.chat.id, "Привет! Могу тебе отправить милого котика!", reply_markup=markup)
- 
- 
-@bot.message_handler(func=lambda message: True)
-def handle_commands(message):
-    if message.text == "Котик":
-        cat_url = get_cat_url()
-        bot.send_photo(message.chat.id, cat_url)
- 
- 
-if __name__ == "__main__":
-    bot.polling(none_stop=True, interval=0)
+
+banan = telebot.TeleBot("6859145466:AAEfzbtPJdcqfvv5p_MUGFmACi1DUQt0jQc")
+
+
+@banan.message_handler(commands=["start"])
+def main(message):
+    banan.send_message(message.chat.id, 'Артем краш')
+
+
+@banan.message_handler(commands=["Да ?"])
+def main(message):
+    banan.send_message(message.chat.id, '*А вы не верили* \n а я уверен в себе', parse_mode='Markdown')
+
+
+@banan.message_handler(commands=["Нет"])
+def main(message):
+    banan.send_message(message.chat.id, 'я хотел сделать выбор Да либо Нет ')
+
+
+banan.infinity_polling()
