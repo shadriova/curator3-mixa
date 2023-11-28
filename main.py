f641d3d06ff445d10aca83988a01afdef9c9a844
Diana <150387973+shadriova@users.noreply.github.com>
2023-11-29 00:45:20 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,24 +1,12 @@
 import telebot
-bot = telebot.TeleBot('6679093324:AAGSrfgrPB_-Lq1uDfAINlBEcYKEkB3KkIs')
+bot = telebot.TeleBot('6755039947:AAEPQR5DGY95hHCSqwhBWu69wWg0lG4txsM')
 
 @bot.message_handler(commands=['start'])
 def main(message):
-    bot.send_message(message.chat.id, '*Привет. \nНачнём общение!*', parse_mode='Markdown')
+    bot.send_message(message.chat.id, 'шанс получить макбук равны *0,00066666666* \n или *1 к 1500*', parse_mode='Markdown')
     
-@bot.message_handler(commands=['deal'])
+@bot.message_handler(commands=['omg'])
 def main(message):
-    bot.send_message(message.chat.id, 'У меня всё хорошо. \nА у тебя?')
-    
-@bot.message_handler(commands=['fine'])
-def main(message):
-    bot.send_message(message.chat.id, 'Рад за тебя!')
-    
-@bot.message_handler(commands=['bedly'])
-def main(message):
-    bot.send_message(message.chat.id, 'Очень жаль...')
-    
-@bot.message_handler(commands=['over'])
-def main(message):
-    bot.send_message(message.chat.id, '*Спасибо за общение!*', parse_mode='Markdown')
-    
+    bot.send_message(message.chat.id, 'это моя реакция если я получу макбук', parse_mode='Markdown')
+
 bot.infinity_polling()
